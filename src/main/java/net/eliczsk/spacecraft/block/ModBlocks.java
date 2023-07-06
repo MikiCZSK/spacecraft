71beefdd867c820eebb06779cbba432961362965
Mikulas <65219648+MikiCZSK@users.noreply.github.com>
2023-07-06 11:28:05 +0200

    Added blocks
    Added aluminium, moon turf

--- src/main/java/net/eliczsk/spacecraft/block/ModBlocks.java
+++ src/main/java/net/eliczsk/spacecraft/block/ModBlocks.java
@@ -3,10 +3,12 @@ package net.eliczsk.spacecraft.block;
 import net.eliczsk.spacecraft.SpaceCraft;
 import net.eliczsk.spacecraft.item.ModCreativeModeTab;
 import net.eliczsk.spacecraft.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -22,9 +24,17 @@ public class ModBlocks {
             DeferredRegister.create(ForgeRegistries.BLOCKS, SpaceCraft.MOD_ID);
     // Deferred register idk...
 
-    //Blocks
+    //Blocks, also needed to make json files in blockstates, models (block and item) and textures
+    public static final RegistryObject<Block> ALUMINIUM_BLOCK = registerBlock("aluminium_block",
+            () -> new Block(BlockBehaviour.Properties.of(Material.METAL).strength(5f)), ModCreativeModeTab.TAB_SPACECRAFT);
+    public static final RegistryObject<Block> ALUMINIUM_ORE = registerBlock("aluminium_ore",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops()),ModCreativeModeTab.TAB_SPACECRAFT);
+    public static final RegistryObject<Block> DEEPSLATE_ALUMINIUM_ORE = registerBlock("deepslate_aluminium_ore",
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(5f).requiresCorrectToolForDrops()),ModCreativeModeTab.TAB_SPACECRAFT);
+
     public static final RegistryObject<Block> MOON_TURF = registerBlock("moon_turf",
-            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(1f)), ModCreativeModeTab.TAB_SPACECRAFT);
+            () -> new Block(BlockBehaviour.Properties.of(Material.STONE).strength(1f).requiresCorrectToolForDrops()), ModCreativeModeTab.TAB_SPACECRAFT);
+
 
 
 
